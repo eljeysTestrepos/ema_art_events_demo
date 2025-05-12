07c00d25c005652e4fa4407ea7ccd811854d9595
eljeys <eljeys1@gmail.com>
2025-05-12 11:12:16 +0200

    handling eventList.map is not a function (error)

--- src/app/(pages)/(visitors)/eventslist/page.jsx
+++ src/app/(pages)/(visitors)/eventslist/page.jsx
@@ -2,13 +2,18 @@ import Basket from "@/app/components/global/Basket";
 import EventItem from "@/app/components/global/EventItem";
 import Filter from "@/app/components/global/Filter";
 import SearchBar from "@/app/components/global/SearchBar";
-
+import { getEvent } from "@/app/lib/api";
 export default function EventsList() {
+  const eventList = getEvent();
+  // console.log(eventList);
   return (
     <main>
       <h1>Eventslist</h1>
       <section>
-        <EventItem />
+        {eventList.map((event) => {
+          console.log(event);
+          return <EventItem key={event.id} {...event} />;
+        })}
       </section>
       <aside>
         <SearchBar></SearchBar>
--- src/app/lib/api.js
+++ src/app/lib/api.js
@@ -1,5 +1,6 @@
 export async function getEvent() {
-  const dataEvents = await fetch("http://localhost:localhost:8080/events");
+  const dataEvents = await fetch("http://localhost:8080/events"); //skift url med eksterne server side når det er deployet
   const dataevent = await dataEvents.json();
+  console.log(dataevent);
   return dataevent;
 }
