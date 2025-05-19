772cc262dec4e26059c622d9841fe77afe786e89
Amanda <amhu0002@stud.kea.dk>
2025-05-19 10:44:53 +0200

    steps

--- src/app/(pages)/(kurator)/create_edit/page.jsx
+++ src/app/(pages)/(kurator)/create_edit/page.jsx
@@ -1,28 +1,12 @@
-import Button from "@/app/components/global/Button";
-import Filter from "@/app/components/global/Filter";
-import SearchBar from "@/app/components/global/SearchBar";
-import EventForm from "@/app/components/kurator_create_edit/EventForm";
-import Gallery from "@/app/components/kurator_create_edit/Gallery";
-import Options from "@/app/components/kurator_create_edit/Options";
-import Step from "@/app/components/kurator_create_edit/Step";
+import Step from "@/components/kurator_create_edit/Step";
 
 export default function Create_Edit() {
   return (
     <main>
-      <h1>Create_Edit</h1>
       <section>
-        <Step />
-        <Options />
-        <Step />
-        <Gallery />
-        <Step />
-        <EventForm />
+        <Step number="1" text="Dato og tid for event" />
       </section>
-      <aside>
-        <SearchBar />
-        <Filter />
-      </aside>
-      <Button />
+      <aside></aside>
     </main>
   );
 }
--- src/components/kurator_create_edit/Step.jsx
+++ src/components/kurator_create_edit/Step.jsx
@@ -1,11 +1,12 @@
 "use client";
 
-const Step = () => {
+const Step = ({ text, number }) => {
   return (
-    <div>
-      <p>2</p>
-      <p>Titel på steppet</p>
-      <p>værk antal men conditional, fordi den ikke skal være mange steder.</p>
+    <div className="flex items-center">
+      <div className="w-16 h-16 flex items-center justify-center border-2 border-text-clr rounded-full">
+        <h1 className="text-text-clr">{number}</h1>
+      </div>
+      <h2 className="ml-4">{text}</h2>
     </div>
   );
 };
