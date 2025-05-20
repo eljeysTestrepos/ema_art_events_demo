e58324ae18c322baacdb1ff7b677573a8c8ec4f6
ElMaez <mai_ez@outlook.dk>
2025-05-20 13:20:39 +0200

    in progress, while making something else

--- src/app/(pages)/(visitors)/events/page.jsx
+++ src/app/(pages)/(visitors)/events/page.jsx
@@ -1,28 +1,18 @@
 import { getEvent } from "@/lib/api";
 
-import Basket from "@/components/global/Basket";
 import EventItem from "@/components/global/EventItem";
 import Filter from "@/components/global/Filter";
 import SearchBar from "@/components/global/SearchBar";
 
 export default async function Events() {
   const eventList = await getEvent();
-  const eventsid = await getEventId();
 
-  const params = await props.params;
-  const searchParams = await props.searchParams;
-  const q = params.q;
-  const query = searchParams.query;
+  // const params = await props.params;
+  // const searchParams = await props.searchParams;
+  // const q = params.q;
+  // const query = searchParams.query;
 
-  console.log(
-    "events page: ",
-    "SMK: ",
-    SMKItems,
-    "eventList: ",
-    eventList,
-    "eventsid: ",
-    eventsid
-  );
+  console.log("events page: ", "SMK: ", SMKItems, "eventList: ", eventList);
 
   console.log("events page: ", "eventList: ", eventList);
 
@@ -37,7 +27,6 @@ export default async function Events() {
       <aside>
         <SearchBar {...SMKItems} {...eventList}></SearchBar>
         <Filter></Filter>
-        <Basket></Basket>
       </aside>
     </main>
   );
