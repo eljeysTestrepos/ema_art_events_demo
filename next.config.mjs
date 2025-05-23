5725eafac87f12ef45d377bf5130c1796301caa9
Elias <Eljeys1@gmail.com>
2025-05-23 18:52:13 +0200

    adding hostname til next.config.js

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "api.smk.dk",
       },
+      {
+        protocol: "https",
+        hostname: "iip-thumb.smk.dk",
+      },
     ],
   },
 };
