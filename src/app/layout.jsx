4dde152c60e1a836cf75fc494787675e33d6f8e0
ElMaez <mai_ez@outlook.dk>
2025-05-25 20:12:35 +0200

    welp

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -12,7 +12,9 @@ export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
       <html lang="en">
+        {/* <motion.div {...anim(opacity)}> */}
         <ThemeWrapper>{children}</ThemeWrapper>
+        {/* </motion.div> */}
       </html>
     </ClerkProvider>
   );
