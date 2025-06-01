1edd33c3900f0542e4f90ceaba002ca0eb07fdc5
Elias <Eljeys1@gmail.com>
2025-06-01 14:06:09 +0200

    moved middleware to root

--- middleware.js
+++ middleware.js
@@ -3,9 +3,12 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 const isProtectedRoute = createRouteMatcher(["/dashboard"]); //Her beskyttes pathes til kurator dashboard (siden vises kun ved login)
 
 //function der spørger til hvor vidt routen matcher en beskyttet route (er det tilfældet den besøgende bliver ledt til en sign in form)
-export default clerkMiddleware(async (auth, req) => {
-  if (isProtectedRoute(req)) await auth.protect();
-});
+export default clerkMiddleware(
+  async (auth, req) => {
+    if (isProtectedRoute(req)) await auth.protect();
+  },
+  { debug: true }
+);
 
 export const config = {
   matcher: [
