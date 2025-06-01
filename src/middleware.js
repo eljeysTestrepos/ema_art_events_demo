197203f9152b470a38daf10626967c89384a2823
ElMaez <mai_ez@outlook.dk>
2025-06-01 15:58:50 +0200

    welp

--- src/middleware.js
+++ src/middleware.js
@@ -1,21 +1,9 @@
-import { authMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import {
+  authMiddleware,
+  clerkMiddleware,
+  createRouteMatcher,
+} from "@clerk/nextjs/server";
 import { NextResponse } from "next/server"; // Importeres for at have den tilgængelig, hvis du får brug for den i fremtiden.
-
-// ---
-
-// Definer de ruter, der skal beskyttes (kræver login)
-// '/dashboard' og '/create_edit' er nu markeret som beskyttede.
-const isProtectedRoute = createRouteMatcher(["/dashboard", "/create_edit"]);
-
-// Definer de ruter, der skal være offentligt tilgængelige (kræver IKKE login)
-const isPublicRoute = createRouteMatcher([
-  "/", // Din hjemmeside
-  "/events", // Oversigt over begivenheder
-  "/api/webhook", // Eksempel: En webhook, der skal være offentlig
-  // Tilføj andre ruter her, der skal være offentlige, f.eks. '/about', '/contact' osv.
-]);
-
-// ---
 
 export default clerkMiddleware();
 
